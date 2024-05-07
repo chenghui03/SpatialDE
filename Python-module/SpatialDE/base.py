1220ff9c3f70c998d26f0b7372afa4fce155f774
chenghui03 <147025572+chenghui03@users.noreply.github.com>
2024-05-07 11:16:24 +0800

    Update base.py
    fix bug : caused by syntax error of df.drop

--- Python-module/SpatialDE/base.py
+++ Python-module/SpatialDE/base.py
@@ -475,7 +475,7 @@ def model_search(X, exp_tab, DE_mll_results, kernel_space=None):
 
     # Retain information from significance testing in the new table
     transfer_columns = ['pval', 'qval', 'max_ll_null']
-    ms_results = ms_results.drop(transfer_columns, 1) \
+    ms_results = ms_results.drop(transfer_columns, axis=1) \
         .merge(DE_mll_results[transfer_columns + ['g']], on='g')
 
     return ms_results
